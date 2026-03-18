ef221cd3cf74c2064a389704484ecd470a6a2e38
komal Aulakh <kkomalpreetkaur@academic.rrc.ca>
2026-03-18 09:44:12 -0500

    switch to komalpreet

---
+++ src/config/helmetConfig.ts
@@ -0,0 +1,7 @@
+import helmet from "helmet";
+
+/**
+ * Helmet configuration for a JSON API.
+ * We are not serving browser-rendered HTML pages, so some browser-focused
+ * protections can be reduced or disabled where appropriate.
+ */
